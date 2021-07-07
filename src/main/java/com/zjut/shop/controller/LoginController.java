d3511c03e2b072bd773276b3fc4b84fadb264e84
liangfutang <liangfu.tang@ecarx.com.cn>
2021-07-07 10:35:43 +0800

    修改登录请求入参名

--- src/main/java/com/zjut/shop/controller/LoginController.java
+++ src/main/java/com/zjut/shop/controller/LoginController.java
@@ -2,7 +2,6 @@ package com.zjut.shop.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,7 +17,7 @@ public class LoginController {
     @PostMapping("/api/private/v1/login")
     public ResponseEntity<?> login(@RequestBody Map<String, String> param) {
         log.info("收到:{}", param);
-        String account = param.get("account");
+        String loginName = param.get("loginName");
 
         JSONObject result = new JSONObject();
         JSONObject data = new JSONObject();
@@ -28,13 +27,13 @@ public class LoginController {
         data.put("mobile", "123");
         data.put("email", "123@qq.com");
         data.put("token", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjUwMCwicmlkIjowLCJpYXQiOjE1MTI1NDQyOTksImV4cCI6MTUxMjYzMDY5OX0.eGrsrvwHm-tPsO9r_pxHIQ5i5L1kX9RX444uwnRGaIM");
-        if (!"cuo".equals(account)) {
+        if (!"cuo".equals(loginName)) {
             result.put("data", data);
         }
         JSONObject meta = new JSONObject();
         meta.put("msg", "登录成功啦");
         meta.put("status", 200);
-        if ("cuo".equals(account)) {
+        if ("cuo".equals(loginName)) {
             meta.put("status", 400);
             meta.put("msg", "登录失败啦");
         }
