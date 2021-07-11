b37bcf2131e4ec735cdc019846afd5fc64e72c4f
liangfutang <tangliangfucom@sina.com>
2021-07-11 23:42:35 +0800

    修改菜单对象

--- src/main/java/com/zjut/shop/controller/HomeController.java
+++ src/main/java/com/zjut/shop/controller/HomeController.java
@@ -1,8 +1,7 @@
 package com.zjut.shop.controller;
 
-import com.zjut.shop.enums.ResultStatus;
 import com.zjut.shop.response.BaseResult;
-import com.zjut.shop.vo.AuthVO;
+import com.zjut.shop.vo.MenuVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -31,7 +30,7 @@ public class HomeController {
     public ResponseEntity<?> menusList(@RequestParam(defaultValue = "0") Integer type) {
         log.info("本次收到的请求类型:{},count:{}", type, count.get());
 
-        List<AuthVO> menus = new ArrayList<>();
+        List<MenuVO> menus = new ArrayList<>();
 
         if (count.get() >= Integer.MAX_VALUE-10) {
             count.set(0);
@@ -54,65 +53,65 @@ public class HomeController {
 
 
         // 用户列表
-        AuthVO userManger = new AuthVO();
+        MenuVO userManger = new MenuVO();
         userManger.setId(110);
-        userManger.setAuthName("用户管理");
+        userManger.setMenuName("用户管理");
         // 用户列表的二级导航
-        List<AuthVO> userChildren = new ArrayList<>();
-        AuthVO oneUser = new AuthVO();
+        List<MenuVO> userChildren = new ArrayList<>();
+        MenuVO oneUser = new MenuVO();
         oneUser.setId(111);
-        oneUser.setAuthName("用户列表");
+        oneUser.setMenuName("用户列表");
         oneUser.setPath("/users");
         userChildren.add(oneUser);
         userManger.setChildren(userChildren);
 
 
         // 权限管理
-        AuthVO rights = new AuthVO();
+        MenuVO rights = new MenuVO();
         rights.setId(130);
-        rights.setAuthName("权限管理");
+        rights.setMenuName("权限管理");
         // 权限管理的二级导航
-        List<AuthVO> rightsChildren = new ArrayList<>();
-        AuthVO one = new AuthVO();
+        List<MenuVO> rightsChildren = new ArrayList<>();
+        MenuVO one = new MenuVO();
         one.setId(130);
-        one.setAuthName("权限列表");
+        one.setMenuName("权限列表");
         one.setPath("/rights");
         rightsChildren.add(one);
         rights.setChildren(rightsChildren);
 
         // 商品管理
-        AuthVO goods = new AuthVO();
+        MenuVO goods = new MenuVO();
         goods.setId(150);
-        goods.setAuthName("商品管理");
+        goods.setMenuName("商品管理");
         // 商品列表的二级导航
-        List<AuthVO> goodsChildren = new ArrayList<>();
-        AuthVO apple = new AuthVO();
+        List<MenuVO> goodsChildren = new ArrayList<>();
+        MenuVO apple = new MenuVO();
         apple.setId(151);
-        apple.setAuthName("苹果");
+        apple.setMenuName("苹果");
         apple.setPath("/goods");
         goodsChildren.add(apple);
-        AuthVO oringe = new AuthVO();
+        MenuVO oringe = new MenuVO();
         oringe.setId(152);
-        oringe.setAuthName("橘子");
+        oringe.setMenuName("橘子");
         oringe.setPath("/api/private/v1/goods/or");
         goodsChildren.add(oringe);
 
-        AuthVO balana = new AuthVO();
+        MenuVO balana = new MenuVO();
         balana.setId(153);
-        balana.setAuthName("香蕉");
+        balana.setMenuName("香蕉");
         balana.setPath("/api/private/v1/goods/bl");
         goodsChildren.add(balana);
         goods.setChildren(goodsChildren);
 
         // 订单管理
-        AuthVO orders = new AuthVO();
+        MenuVO orders = new MenuVO();
         orders.setId(170);
-        orders.setAuthName("订单管理");
+        orders.setMenuName("订单管理");
 
         // 数据统计
-        AuthVO reports = new AuthVO();
+        MenuVO reports = new MenuVO();
         reports.setId(190);
-        reports.setAuthName("数据统计");
+        reports.setMenuName("数据统计");
 
         menus.add(userManger);
         menus.add(rights);
--- src/main/java/com/zjut/shop/vo/MenuVO.java
+++ src/main/java/com/zjut/shop/vo/MenuVO.java
@@ -2,7 +2,32 @@ package com.zjut.shop.vo;
 
 import lombok.Data;
 
+import java.util.List;
+
+/**
+ * 菜单
+ */
 @Data
 public class MenuVO {
 
+    private Integer id;
+
+    private String menuName;
+
+    /**
+     * 权限等级
+     */
+    private String level;
+
+    /**
+     * 父权限id
+     */
+    private Integer pid;
+
+    private String path;
+
+    /**
+     * 子权限
+     */
+    private List<MenuVO> children;
 }
